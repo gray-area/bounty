ea90589fe0fbc0d849abaaf0e8dbdaf8a6c48ad6
gray-area <91284249+gray-area@users.noreply.github.com>
2024-01-12 12:00:45 -0500

    Update install.sh
    typo fix in path

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ function purple(){
 }
 
 #Install Go
-cd Downlaods
+cd ~/Downloads
 wget https://golang.org/dl/go1.20.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
 echo "export PATH=/usr/local/go/bin:${PATH}" | sudo tee -a $HOME/.profile
