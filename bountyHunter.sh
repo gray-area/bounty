c81786e66214f11366d1cbdc950f3a291ffc9cbf
gray-area <91284249+gray-area@users.noreply.github.com>
2024-02-12 14:52:54 -0500

    Update bountyHunter.sh
    bbrf

--- bountyHunter.sh
+++ bountyHunter.sh
@@ -123,6 +123,21 @@ echo
 echo "Running Recon..."
 echo
 
+#Placeholder for bbrf
+#bbrf new $url
+#bbrf use $url
+#bbrf inscope add '*.$url'
+#bbrf scope in --wildcard --top | subfinder | bbrf domain add - --show-new
+#for p in $(bbrf programs); do
+#  bbrf domains --view unresolved -p $p | \
+#  dnsx -silent -a -resp | tr -d '[]' | tee \
+#      >(awk '{print $1":"$2}' | bbrf domain update - -p $p -s dnsx) \
+#      >(awk '{print $1":"$2}' | bbrf domain add - -p $p -s dnsx) \
+#      >(awk '{print $2":"$1}' | bbrf ip add - -p $p -s dnsx) \
+#      >(awk '{print $2":"$1}' | bbrf ip update - -p $p -s dnsx)
+#done
+#bbrf domains --view resolved
+
 purple "[+] Bruteforcing directories with ffuf..."
 (ffuf -u https://$url/FUZZ -w /usr/share/wordlists/seclists/Disocvery/Web-Content/directory-list-lowercase-2.3-small.txt -fc 404 -recursion -recursion-depth 2 -o $url/recon/ffuf.txt) &
 spinner $!
